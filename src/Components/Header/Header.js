e0df1f053222293666563e8178fe8ee1e3470147
Hồ Viết Việt Khánh <98158225+vietkhanhvn@users.noreply.github.com>
2023-01-04 18:00:30 +0700

    Update Header.js

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -28,7 +28,7 @@ function Header() {
       <header className={styles.header}>
         {IsNotMobile() && (
           <a href="#home" className={styles.title}>
-            <h1 className={styles}>Quoc Huy</h1>
+            <h1 className={styles}>Ho Viet Viet Khanh</h1>
           </a>
         )}
         <div className={styles.changeTheme}>
